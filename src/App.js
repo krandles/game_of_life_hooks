de3c311c83cc22ddee4f0a29cd788013e3357b96
krandles <krandles@gmail.com>
2019-07-09 01:44:23 -0400

    pushing to github

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Board from '../src/components/Board'
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <h1>Conway's Game of Life</h1>
+      </header>
+      <Board/>
     </div>
   );
 }
